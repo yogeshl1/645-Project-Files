e25148675309af3ce8392871bb123304f416cf60
Yogesh Lakkaraju <lakkarajuyogesh4061@gmail.com>
2023-11-09 20:01:15 -0500

    commit

---
+++ RetrofitClientInstance.java
@@ -0,0 +1,15 @@
+public class RetrofitClientInstance {
+
+    private static Retrofit retrofit;
+    private static final String BASE_URL = "http://localhost:3000/";
+
+    public static Retrofit getRetrofitInstance() {
+        if (retrofit == null) {
+            retrofit = new retrofit2.Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
